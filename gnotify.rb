109e4bdcca2f055ae62bab3411c9107624a193c3
arohee <anicetrohee@gmail.com>
2009-10-22 10:51:35 +0200

    pnotify and gnotify for weechat 3.0

--- gnotify.rb
+++ gnotify.rb
@@ -1,5 +1,5 @@
 # - gnotify.rb 
-# pnotify.rb is a script for Weechat that sends highlight notifications and private messages to the Growl notification framework. 
+# gnotify.rb is a script for Weechat that sends highlight notifications and private messages to the Growl notification framework. 
 # see http://growl.info/
 # It uses the ruby gem meow.
 # 
@@ -8,81 +8,35 @@
 # mkdir ~/.weechat/images/
 # cp images/weechat.png ~/.weechat/images/
 
-def load_meow
- #for mac os x only Snow Leopard
- $: <<
-   '/System/Library/Frameworks/Ruby.framework/Versions/1.8/usr/lib/ruby/1.8' <<
-   '/System/Library/Frameworks/Ruby.framework/Versions/1.8/usr/lib/ruby/1.8/universal-darwin10.0'
-
- require 'stringio'
- require 'rubygems'
-
- # TODO Handle the error more quitly
- old_stdout = $stdout
- $stdout = StringIO.new
- gem 'meow'
- require 'meow'
- $stdout = old_stdout
-end
+#for mac os x only
+# $: <<
+#   '/System/Library/Frameworks/Ruby.framework/Versions/1.8/usr/lib/ruby/1.8' <<
+#   '/System/Library/Frameworks/Ruby.framework/Versions/1.8/usr/lib/ruby/1.8/universal-darwin10.0'
+ 
+require 'stringio'
+require 'rubygems'
+gem 'meow'
+require 'meow'
 
 def weechat_init
- Weechat.register "gnotify", "0.1", "", "Notify user via growl"
- load_meow
- image = Meow.import_image File.expand_path("~/.weechat/images/weechat.png")
- @meow = Meow.new('WeeChat', 'Note', image)
- Weechat.add_message_handler("privmsg", "notify")
- Weechat.add_message_handler("weechat_highlight", "notify_highlight")
- return Weechat::PLUGIN_RC_OK
+  Weechat.register("gnotify", "plop@example.com","0.2", "GPL3", "Notify user via growl", "", "") 
+  image = Meow.import_image File.expand_path("~/.weechat/images/weechat.png")
+  @meow = Meow.new('WeeChat', 'Note', image)
+  Weechat.hook_signal("weechat_pv", "meow_private_message", "")
+  Weechat.hook_signal("weechat_highlight", "meow_highlight", "")
+  return Weechat::WEECHAT_RC_OK
 end
 
-def get_nickname(user)
- user =~ /^:([^!]+)/
- $1
+def meow_highlight(data,signal,msg)
+  title = "meow_highlight"
+  message = "#{msg}"
+  @meow.notify(title, message)
+  return Weechat::WEECHAT_RC_OK
 end
 
-def split_args(args)
- args = args.split(/\s+/, 4)
- args[-1] = args.last[1..-1]
- args
-end
-
-def notify(server, args)
- # args = args.split(/\s+/, 4)
- # Weechat.print args.inspect
- # from = get_nickname args.first
- # dest = args[3]
- # msg = args.last[1..-1]
- # from, action, dest, msg = args.split(/\s+/, 4)
- from, action, dest, msg = split_args args
- from = get_nickname from
- # Weechat.print [from, action, dest, msg].inspect
- if dest[0] == ?#
-   # Do nothing
-   # Weechat.print "Public msg"
- else
-   meow_private_message from, msg
- end
- return Weechat::PLUGIN_RC_OK
-end
-
-def notify_highlight(server, args)
- # Weechat.print [server, args].inspect
- from, action, dest, msg = split_args args
- from = get_nickname from
- meow_highlight from, dest, msg
- return Weechat::PLUGIN_RC_OK
-end
-
-def meow_highlight(from, nickname, message)
- title = "Highlight on #{nickname}"
- # TODO Add truncate
- message = "#{from}: #{message}"
- @meow.notify(title, message)
- return Weechat::PLUGIN_RC_OK
-end
-
-def meow_private_message(nickname, message)
- title = "Privmsg from #{nickname}"
- # TODO Add truncate
- @meow.notify(title, message)
+def meow_private_message(data,signal,msg)
+  title = "meow_private_message"
+  message = "#{msg}"
+  @meow.notify(title, message)
+  return Weechat::WEECHAT_RC_OK
 end
--- pnotify.rb
+++ pnotify.rb
@@ -6,16 +6,15 @@
 # sudo gem install prowl
 # cp pnotify.rb ~/.weechat/ruby/autoload/
 
-
 def load_prowl
- #for mac os x only Snow Leopard
- $: <<
-   '/System/Library/Frameworks/Ruby.framework/Versions/1.8/usr/lib/ruby/1.8' <<
-   '/System/Library/Frameworks/Ruby.framework/Versions/1.8/usr/lib/ruby/1.8/universal-darwin10.0'
-
  require 'stringio'
  require 'rubygems'
  require 'prowl'
+
+ #for mac os x only
+ $: <<
+   '/System/Library/Frameworks/Ruby.framework/Versions/1.8/usr/lib/ruby/1.8' <<
+   '/System/Library/Frameworks/Ruby.framework/Versions/1.8/usr/lib/ruby/1.8/universal-darwin10.0'
 
  # TODO Handle the error more quitly
  old_stdout = $stdout
@@ -24,59 +23,30 @@ def load_prowl
 end
 
 def weechat_init
- Weechat.register "pnotify", "0.1", "", "Notify user via prowl"
+ Weechat.register("pnotify","plop@example.com", "0.2", "GPL3", "Notify user via prowl", "", "")
  load_prowl
- Weechat.add_message_handler("privmsg", "notify")
- Weechat.add_message_handler("weechat_highlight", "notify_highlight")
- return Weechat::PLUGIN_RC_OK
+ Weechat.hook_signal("weechat_pv","prowl_private_message", "")
+ Weechat.hook_signal("weechat_highlight", "prowl_highlight", "")
+ return Weechat::WEECHAT_RC_OK
 end
 
-def get_nickname(user)
- user =~ /^:([^!]+)/
- $1
-end
 
-def split_args(args)
- args = args.split(/\s+/, 4)
- args[-1] = args.last[1..-1]
- args
-end
-
-def notify(server, args)
- from, action, dest, msg = split_args args
- from = get_nickname from
- if dest[0] == ?#
-   # Do nothing
-   # Weechat.print "Public msg"
- else
-   prowl_private_message from, msg
- end
- return Weechat::PLUGIN_RC_OK
-end
-
-def notify_highlight(server, args)
- # Weechat.print [server, args].inspect
- from, action, dest, msg = split_args args
- from = get_nickname from
- prowl_highlight from, dest, msg
- return Weechat::PLUGIN_RC_OK
-end
-
-def prowl_highlight(from, nickname, message)
- title = "Highlight on #{nickname}"
- # TODO Add truncate
- message = "#{from}: #{message}"
+def prowl_highlight(data, signal, msg)
+ title = "Highlight on #{data} #{signal}"
+ message = "#{msg}"
  send_prowl(title,message)
+ return Weechat::WEECHAT_RC_OK
 end
 
-def prowl_private_message(nickname, message)
- title = "Private message : #{nickname}" 
- # TODO Add truncate
+def prowl_private_message(data, signal, msg)
+ title = "Private message #{data} #{signal}:" 
+ message = "#{msg}" 
  send_prowl(title,message)
+ return Weechat::WEECHAT_RC_OK
 end
 
 def send_prowl(title, message)
-  Prowl.add("your apikey here", {
+  Prowl.add("your api key here", {
       :application => "Weechat",
       :event => title,
       :description => message
